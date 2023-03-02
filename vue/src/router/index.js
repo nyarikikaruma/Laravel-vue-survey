da57b5d13aed8e5efdf55354cb4ca880683be142
unknown <nyarikikaruma@gmail.com>
2023-03-02 23:48:20 +0300

    Default layout added

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,30 +2,67 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
+import AuthLayout from '../components/AuthLayout.vue'
+import Surveys from '../views/Surveys.vue'
+import store from "../store";
 
 
 const routes = [
     {
       path: '/',
-      name: 'dashboard',
-      component: Dashboard
+      redirect: '/dashboard',
+      meta: {requireAuth: true},  
+      component: DefaultLayout,
+      children: [
+        {
+          path: '/dashboard',
+          name: 'dashboard',
+          component: Dashboard
+        },
+        {
+          path: '/surveys',
+          name: 'surveys',
+          component: Surveys
+        }
+      ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
-      },
-      {
-        path: '/register',
-        name: 'register',
-        component: Register
-      },
-    
-];
+      path: '/auth',
+      redirect: '/login',
+      name: 'Auth',
+      meta: {isGuest: true},
+      component: AuthLayout,
+      children: [
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+          },
+          {
+            path: '/register',
+            name: 'register',
+            component: Register
+          },
+
+      ]
+    }
+  ]
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requireAuth && !store.state.user.token){
+    next({name: 'Login'})
+  }
+  else if(store.state.user.token && (to.meta.isGuest)){
+    next({name: 'Dashboard'});
+  }else{
+    next();
+  }
 })
 
 export default router
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,35 +1,11 @@
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
-    <!--
-      This example requires updating your template:
-  
-      ```
-      <html class="h-full bg-gray-50">
-      <body class="h-full">
-      ```
-    -->
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="w-full max-w-md space-y-8">
         <div>
           <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
           <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
           <p class="mt-2 text-center text-sm text-gray-600">
             Or
             {{ ' ' }}
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+            <a href="#/register" class="font-medium text-indigo-600 hover:text-indigo-500">Register for free</a>
           </p>
         </div>
         <form class="mt-8 space-y-6" action="#" method="POST">
@@ -51,9 +27,6 @@
               <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
             </div>
   
-            <div class="text-sm">
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
-            </div>
           </div>
   
           <div>
@@ -65,8 +38,6 @@
             </button>
           </div>
         </form>
-      </div>
-    </div>
   </template>
   
   <script setup>
