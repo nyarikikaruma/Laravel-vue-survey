6de45cae944bb7757684660316cd60482f6efb01
unknown <nyarikikaruma@gmail.com>
2023-03-02 09:12:22 +0300

    First commit

---
+++ vue/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import './index.css'
+import store from './store'
+import router from './router'
+import App from './App.vue'
+
+createApp(App).use(router).use(store).mount('#app')
